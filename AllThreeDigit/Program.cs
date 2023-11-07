3e4acf88ec560c10df6f7228c2d3cf69b14ece79
ИСП-22 <-22@LAB30-09>
2023-11-07 08:26:57 +0300

    Добавьте файлы проекта.

---
+++ AllThreeDigit/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Все трехзначные числа, в десятичной записи в которых нет одинаковых цифр.");
+
+    for (int i = 100; i <= 999; i++)
+        {
+            int number = i;
+            int digit1 = number % 10;
+            number /= 10;
+            int digit2 = number % 10;
+            number /= 10;
+            int digit3 = number % 10;
+
+            if (digit1 != digit2 && digit1 != digit3 && digit2 != digit3)
+            {
+                Console.WriteLine(i);
+            }
+        }
